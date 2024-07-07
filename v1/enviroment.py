9166747749e114c0f76e3d60048e4fbec41e6038
alfadefender <serejenk4.m@yandex.ru>
2024-07-07 23:39:26 +0500

    Update enviroment.py

--- v1/enviroment.py
+++ v1/enviroment.py
@@ -1,8 +1,9 @@
-import pygame
+import pygame, random
 from settings import *
 
 class Objects:
-    def __init__(self, position, isCollidable, health=None, texture=None, color=(50, 100, 200)):
+    def __init__(self, window, position, isCollidable, health=None, texture=None, color=(50, 100, 200)):
+        self.window = window
         self.texture = texture
         self.health = health
         self.isCollidable = isCollidable
@@ -19,10 +20,10 @@ class Objects:
     def Draw(self):
         if (self.health != 0):
             if self.texture == None:
-                pygame.draw.circle(window, self.color, self.position, 10)
+                pygame.draw.circle(self.window, self.color, self.position, 10)
 
             else:
-                window.blit(self.texture, self.position)
+                self.window.blit(self.texture, self.position)
 
     def Collidepoint(self, entities):
         if self.isCollidable:
@@ -32,7 +33,8 @@ class Objects:
         pass
 
 class Location:
-    def __init__(self, hero, objects=[], texture=None, entities=[], color=(0,0,0)):
+    def __init__(self, window, hero, objects=[], texture=None, entities=[], color=(0,0,0)):
+        self.window = window
         self.obiects = objects
         self.texture = texture
         self.doors = []
@@ -45,11 +47,11 @@ class Location:
             for element in range(random.randint(1, 3)):
                 tempChoice = random.choice("awd")
                 if tempChoice == "a":
-                    self.doors += [Door([0, resolution[1] / 2], 1)]
+                    self.doors += [Door(self.window, self.hero, [0, resolution[1] / 2], 1)]
                 elif tempChoice == "w":
-                    self.doors += [Door([resolution[0] / 2, 0], 2)]
+                    self.doors += [Door(self.window, self.hero,[resolution[0] / 2, 0], 2)]
                 else:
-                    self.doors += [Door([resolution[0], resolution[1] / 2], 3)]
+                    self.doors += [Door(self.window, self.hero,[resolution[0], resolution[1] / 2], 3)]
 
             for door in self.doors:
                 door.MakeVisible()
@@ -57,10 +59,10 @@ class Location:
 
     def Draw(self):
         if self.texture != None:
-            window.blit(self.texture, (0, 0))
+            self.window.blit(self.texture, (0, 0))
 
         else:
-            window.fill(self.color)
+            self.window.fill(self.color)
 
         for door in self.doors:
             door.Draw()
@@ -91,11 +93,17 @@ class Location:
             self.MakeDoors()
 
         for door in self.doors:
-            door.PlayerCollidepoint()
+            changeLocation = door.PlayerCollidepoint()
+            if changeLocation != None:
+                return changeLocation
+
+        return None
 
 # класс дверей
 class Door:
-    def __init__(self, position, side, texture=None, isVisible=False):
+    def __init__(self, window, hero, position, side, texture=None, isVisible=False):
+        self.window = window
+        self.hero = hero
         self.texture = texture
         self.position = position
         self.side = side
@@ -106,20 +114,21 @@ class Door:
     def Draw(self):
         if self.isVisible:
             if self.texture != None:
-                window.blit(self.texture, self.position)
+                self.window.blit(self.texture, self.position)
 
             else:
-                pygame.draw.circle(window, (200, 200, 200), self.position, 50)
+                pygame.draw.circle(self.window, (200, 200, 200), self.position, 50)
 
     def MakeVisible(self):
         if not self.isVisible:
             self.isVisible = True
 
     def PlayerCollidepoint(self):
-        global changeLocation
-        if self.isVisible and self.hitbox.collidepoint(hero.position[0], hero.position[1]):
+        if self.isVisible and self.hitbox.collidepoint(self.hero.position[0], self.hero.position[1]):
             self.isVisible = False
-            changeLocation = self.side
+            return self.side
+
+        return None
 
 class Item:
     pass
